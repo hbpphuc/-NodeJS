108227a9f36661ec0b9820c154aeade6b7a451bf
Phuc <hbphuc264@gmail.com>
2022-03-17 12:41:27 +0700

    Refactor code: create controller and route

--- src/index.js
+++ src/index.js
@@ -6,23 +6,25 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({ extname: '.hbs' })
 
+const route = require('./routes')
+
 // TEMPLATE ENGINE
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, 'resources/views'));
 
 //HTTP LOGGER
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+//Route init
+route(app)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
